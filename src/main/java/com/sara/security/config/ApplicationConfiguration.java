f6b884f2ce709676262d7003a40d8ce553b605e6
k4kranti <k4kranti@gmail.com>
2019-09-04 16:22:26 -0400

    adding the http authorization

--- src/main/java/com/sara/security/config/ApplicationConfiguration.java
+++ src/main/java/com/sara/security/config/ApplicationConfiguration.java
@@ -3,7 +3,7 @@ package com.sara.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,15 +12,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class ApplicationConfiguration  extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.authorizeRequests()
+               .antMatchers("/secure/admin").hasRole("ADMIN")
+               .antMatchers("/secure/user").hasAnyRole("USER","ADMIN")
+               .antMatchers("/**").permitAll()
+               .and()
+               .formLogin();
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("user")
-                .roles("USER")
+                .withUser("user").password("user").roles("USER")
                 .and()
-                .withUser("admin")
-                .password("admin")
-                .roles("ADMIN");
+                .withUser("admin").password("admin").roles("ADMIN");
     }
 
     @Bean
--- src/main/java/com/sara/security/controller/HomeResource.java
+++ src/main/java/com/sara/security/controller/HomeResource.java
@@ -12,4 +12,14 @@ public class HomeResource {
     public ResponseEntity<?> home() {
         return new ResponseEntity<>("<h1> Hello Welcome! </h1>", HttpStatus.OK);
     }
+
+    @GetMapping(path = "/secure/user")
+    public ResponseEntity<?> user() {
+        return new ResponseEntity<>("<h1> Hello Welcome User! </h1>", HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/secure/admin")
+    public ResponseEntity<?> admin() {
+        return new ResponseEntity<>("<h1> Hello Welcome Admin! </h1>", HttpStatus.OK);
+    }
 }
